b44b79c3df2661dc09d1aac3b6f1287dbc1509f1
gggggg ffffff <gggggg ffffff>
2016-06-08 17:39:09 +0800

    no message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("index,helloword,ITWORKS!");
+  res.send("index,helloword,ITWORKS!23333");
 });
 
 module.exports = router;
