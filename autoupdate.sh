760d4aa9f07e3be46e8669878953c20ef2be0aa2
gggggg ffffff <gggggg ffffff>
2016-06-08 17:11:02 +0800

    no message

---
+++ autoupdate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+WEB_PATH='/home/poweruser/acgoped'
+WEB_USER='poweruser'
+WEB_USERGROUP='poweruser'
+ 
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+git reset --hard origin/master
+git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
