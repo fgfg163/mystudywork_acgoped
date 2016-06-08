c7b2feb01b44701800638fb4fb31d87ff7f6b327
gggggg ffffff <gggggg ffffff>
2016-06-08 17:36:38 +0800

    no message

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
- 
+unset GIT_DIR
 WEB_PATH='/home/poweruser/acgoped'
 WEB_USER='poweruser'
 WEB_USERGROUP='poweruser'
@@ -13,4 +13,5 @@ git pull
 git checkout master
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+forever restartall
 echo "Finished."
